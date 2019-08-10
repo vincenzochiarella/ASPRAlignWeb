cf937cda24c1335f9a19b0b5fc8efad3bc242bb7
Vincenzo Chiarella <vins14nov@outlook.it>
2019-08-10 16:40:56 +0200

    process build

--- backend/src/builder/stringBuilder.js
+++ backend/src/builder/stringBuilder.js
@@ -7,11 +7,13 @@ const parser = require('./stringAnalizer')
 
 module.exports.start = Elaboration = (options, input) => {
     return new Promise((resolve, reject) => {
-        fs.writeFile(pathFolder + '/Test1.txt', input, (err) => {
-            if (err) {
-                return console.log(err)
-            }
-        })
+        //da cancellare appena elaborato (oppure chiedere se lasciarlo)
+        // fs.writeFile(pathFolder + '/Test1.txt', input, (err) => {
+        //     if (err) {
+        //         return console.log(err)
+        //     }
+        // })
+
         const JavaProcess = require('child_process').spawn('java', ['-jar', pathFolder + '\\ASPRAlign.jar', '-s', pathFolder + '\\Test1.txt'])
         console.log(JavaProcess.pid)
 
@@ -25,4 +27,45 @@ module.exports.start = Elaboration = (options, input) => {
             reject(err);
         });
     })
+}
+
+optionsSwitch = ( options, input, id ) => { 
+    var optionsRequested = ['-jar', pathFolder + '\\ASPRAlign.jar']
+    if(options.aasinput) {
+        optionsRequested = optionsRequested.concat('-r'),
+    }
+    if(options.align){
+        optionsRequested = optionsRequested.concat('-a'),
+        optionsRequested = optionsRequested.concat(temporaryFile(input[0],1))
+        optionsRequested = optionsRequested.concat(temporaryFile(input[1],2))
+        if(options.chkpair) optionsRequested = optionsRequested.concat('-c')
+        if(options.outdist) optionsRequested = optionsRequested.concat('-d')
+        if(options.showscores) optionsRequested = optionsRequested.concat('-e')
+    } else if( options.alg ){
+        optionsRequested = optionsRequested.concat('-g')
+        optionsRequested = optionsRequested.concat(temporaryFile(input[0],1))
+    
+    } else if( options.struct ){
+        optionsRequested = optionsRequested.concat('-s')
+        optionsRequested = optionsRequested.concat(temporaryFile(input[0],1))
+    }
+    if( options.latexout){
+        optionsRequested = optionsRequested.concat('-l')
+    }
+    if( options.useconffile){
+        optionsRequested = optionsRequested.concat('-l')
+    }
+    return optionsRequested
+}
+temporaryFile = ( input , inc ) => {
+    //da cancellare appena finita l'elaborazione
+    fs.writeFile(pathFolder + `/Test_${inc}.txt`, input, (err) => {
+        if (err) {
+            return console.log(err)
+        }
+    })
+    return pathFolder + `/Test_${inc}.txt`
+}
+temporaryConffile = ( input ) =>{
+
 }
